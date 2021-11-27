7555b49b26a25cda7ac05b5f34d8072e0d0acea5
Social-Interaction <86463867+Social-Interaction@users.noreply.github.com>
2021-11-26 19:19:00 -0800

    message test

--- Assets/resource_manifest2.js
+++ Assets/resource_manifest2.js
@@ -19,7 +19,7 @@ const resources = {
 	"district": [
 		"new Resource("District Bylaws", "pdf", "http://resources.cnhcirclek.org/Documents/District/CNH Circle K District Bylaws Updated 05.02.2019.pdf")",
 		"new Resource("District Bylaws 2", "link", "http://resources.cnhcirclek.org/Documents/District/CNH Circle K District Bylaws Updated 05.02.2019.pdf")",
-		"new Dropdown("daddy", "peppapig", [\n\t"new Resource("ja", "pdf", "frog")"\n])"
+		"new Dropdown("daddy", "peppapig", [\n\t"new Resource("frog", "pdf", "http://resources.cnhcirclek.org/Documents/District/CNH Circle K District Bylaws Updated 05.02.2019.pdf")"\n])"
 	],
 	"divisions": [
 		"new Resource("Tomatoes", "info", "http://resources.cnhcirclek.org/Documents/District/CNH Circle K District Bylaws Updated 05.02.2019.pdf")"
